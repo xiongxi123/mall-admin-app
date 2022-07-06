1fdb3583a50027d6de15bdd6761a56b42a969417
studentxiong <1296861068@qq.com>
2022-07-06 20:24:02 +0800

    代码优化

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,19 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    // 用于切换菜单的闭合状态  false 代表不闭合  true代表闭合
+    collapsed: false,
+  },
+  mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
+  },
+  actions: {
+    changeCollapsed({ commit }) {
+      commit("changeCollapsed");
+    },
+  },
   modules: {},
 });
