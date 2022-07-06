8a9ddf30bd114ddc2e7040a62f9f523d94d4625a
studentxiong <1296861068@qq.com>
2022-07-06 16:07:17 +0800

    系统初始布局

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false, // 关闭eslint校验
+});
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+};
