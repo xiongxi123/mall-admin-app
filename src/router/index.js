06a64e38b0963dc2167601b37aa2ba664b7dce25
studentxiong <1296861068@qq.com>
2022-07-07 12:51:46 +0800

    菜单栏渲染

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,8 @@ const ayncRouterMap = [
     name: "Product",
     meta: {
       title: "商品",
+      icon: "inbox",
+      hidden: false,
     },
     component: Home,
     children: [
@@ -20,6 +22,8 @@ const ayncRouterMap = [
         name: "ProductList",
         meta: {
           title: "商品列表",
+          icon: "unordered-list",
+          hidden: false,
         },
         component: () => import("@/views/page/productList.vue"),
       },
@@ -28,6 +32,8 @@ const ayncRouterMap = [
         name: "ProductAdd",
         meta: {
           title: "添加商品",
+          icon: "file-add",
+          hidden: false,
         },
         component: () => import("@/views/page/productAdd.vue"),
       },
@@ -36,6 +42,8 @@ const ayncRouterMap = [
         name: "Category",
         meta: {
           title: "类目管理",
+          icon: "project",
+          hidden: false,
         },
         component: () => import("@/views/page/category.vue"),
       },
@@ -50,6 +58,8 @@ const routes = [
     component: Home,
     meta: {
       title: "首页",
+      hidden: false,
+      icon: "home",
     },
     children: [
       {
@@ -57,6 +67,8 @@ const routes = [
         name: "Index",
         meta: {
           title: "统计",
+          icon: "number",
+          hidden: false,
         },
         component: () => import("../views/page/index.vue"),
       },
@@ -68,6 +80,7 @@ const routes = [
     component: Login,
     meta: {
       title: "登录",
+      hidden: true,
     },
   },
 ];
@@ -81,8 +94,10 @@ router.beforeEach((to, from, next) => {
     if (store.state.user.appkey && store.state.user.username && store.state.user.role) {
       if (!isAddRoutes) {
         const menuRoutes = getMenuRoutes(store.state.user.role, ayncRouterMap);
-        router.addRoutes(menuRoutes);
-        store.dispatch("changeMenuRoutes", routes.concat(menuRoutes));
+        store.dispatch("changeMenuRoutes", routes.concat(menuRoutes)).then(() => {
+          router.addRoutes(menuRoutes);
+          next();
+        });
         isAddRoutes = true;
       }
       return next();
