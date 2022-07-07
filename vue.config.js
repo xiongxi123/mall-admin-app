74e1667ccc2b7b8385d606ac5491b73fb71ac13e
studentxiong <1296861068@qq.com>
2022-07-07 21:35:12 +0800

    商品列表表格渲染

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  // devServer: {
+  //   host: '0.0.0.0',
+  //   //https:true,
+  //   port: 8080,
+  //   client: {
+  //     webSocketURL: 'ws//0.0.0.0:8080/ws',
+  //   },
+  //   header: {
+  //     'Access-Control-Allow-Origin':'*',
+  //   }
+  // },
   transpileDependencies: true,
   lintOnSave: false, // 关闭eslint校验
 });
@@ -12,20 +23,20 @@ module.exports = {
       },
     },
   },
-  devServer: {
-    proxy: {
-      '/': {
-        ws: false, //proxy websockets
-        target: 'http://192.168.11.172:8090',
-        pathRewrite: {
-          '^/':'/bpp'
-        },
-        cookiePathRewrite: {
-          'bpp':'/'
-        }
-      }
-    }
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       ws: false, //proxy websockets
+  //       target: 'http://192.168.101.8:8080',
+  //       pathRewrite: {
+  //         '^/':'/bpp'
+  //       },
+  //       cookiePathRewrite: {
+  //         'bpp':'/'
+  //       }
+  //     }
+  //   }
+  // },
   // baseUrl:process.env.NODE_ENV === 'production'?'/bpp':'/'
 };
 
