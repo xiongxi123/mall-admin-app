f395ca84867999445aade2b75e522fe667afe28b
studentxiong <1296861068@qq.com>
2022-07-10 13:29:59 +0800

    编辑功能完成

--- src/components/basicDetail.vue
+++ src/components/basicDetail.vue
@@ -1,7 +1,8 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
   <div class="basic-detail">
-    <a-form-model ref="ruleForm" :model="form" :rules="rules" :label-col="{ span: 5 }" :wrapper-col="{ span: 13 }">
+    <a-form-model ref="ruleForm" :model="form" :rules="rules"
+        :label-col="{span: 5}" :wrapper-col="{span: 13}">
       <a-form-model-item label="标题" prop="title" required>
         <a-input v-model="form.title" />
       </a-form-model-item>
@@ -10,23 +11,31 @@
       </a-form-model-item>
       <a-form-model-item label="商品类目" required prop="category">
         <a-select v-model="form.category" placeholder="请选择商品类别" @change="changeCategory">
-          <a-select-option v-for="c in categoryList" :key="c.id" :value="c.id">
-            {{ c.name }}
-          </a-select-option>
+          <a-select-option v-for="c in categoryList" :key="c.id" :value="c.id"
+                           >
+              {{c.name}}
+            </a-select-option>
         </a-select>
-        <a-select v-model="form.c_items" placeholder="请添加子类目">
+        <a-select v-model="form.c_item" placeholder="请添加子类目">
           <a-select-option v-for="c in categoryItems" :key="c" :value="c">
-            {{ c }}
-          </a-select-option>
+            {{c}}
+        </a-select-option>
         </a-select>
       </a-form-model-item>
       <a-form-model-item label="商品标签" prop="tags" required>
-        <a-select mode="tags" placeholder="Please select" :default-value="['包邮，最晚次日达']" v-model="form.tags">
-          <a-select-option value="包邮，最晚次日达">包邮，最晚次日达</a-select-option>
+        <a-select
+          mode="tags"
+          placeholder="Please select"
+          :default-value="['包邮，最晚次日达']"
+          v-model="form.tags"
+        >
+          <a-select-option
+            value="包邮，最晚次日达"
+          >包邮，最晚次日达</a-select-option>
         </a-select>
       </a-form-model-item>
-      <a-form-model-item label="" class="next-btn" :wrapperCol="{ span: 24 }">
-        <a-button type="primary" @click="next">下一步</a-button>
+      <a-form-model-item label="" class="next-btn" :wrapperCol="{span: 24}">
+          <a-button type="primary" @click="next">下一步</a-button>
       </a-form-model-item>
     </a-form-model>
   </div>
@@ -37,13 +46,13 @@ import categoryApi from '@/api/category';
 export default {
   data() {
     return {
-        // form: {
-        //   title: '',
-        //   desc: '',
-        //   category: '',
-        //   c_items: [],
-        //   tags: [],
-        // },
+    //   form: {
+    //     title: '',
+    //     desc: '',
+    //     category: '',
+    //     c_items: [],
+    //     tags: [],
+    //   },
       rules: {
       },
       categoryList: [],
@@ -51,13 +60,6 @@ export default {
     };
   },
   props: ['form'],
-  // props: {
-  //   form: {
-  //     type: Boolean,
-  //     required: true,
-  //     default: false,
-  //   }
-  // },
   created() {
     categoryApi.list().then((res) => {
       this.categoryList = res.data;
@@ -87,9 +89,9 @@ export default {
 </script>
 
 <style lang="less">
-.basic-detail {
-  .next-btn {
-    text-align: center;
-  }
-}
+    .basic-detail {
+        .next-btn {
+            text-align: center;
+        }
+    }
 </style>
--- src/components/saleDetail.vue
+++ src/components/saleDetail.vue
@@ -1,13 +1,7 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
   <div class="sale-detail">
-    <a-form-model
-      ref="ruleForm"
-      :model="form"
-      :rules="rules"
-      :label-col="{span: 5}"
-      :wrapper-col="{span: 13}"
-    >
+    <a-form-model ref="ruleForm" :model="form" :rules="rules" :label-col="{span: 5}" :wrapper-col="{span: 13}">
       <a-form-model-item label="商品售价" prop="price" required>
         <a-input v-model="form.price" />
       </a-form-model-item>
@@ -21,14 +15,8 @@
         <a-input v-model="form.unit" />
       </a-form-model-item>
       <a-form-model-item label="商品相册" prop="images">
-        <a-upload
-          :action="'http://mallapi.duyiedu.com/upload/images?appkey=' + $store.state.user.appkey"
-          list-type="picture-card"
-          :file-list="fileList"
-          @preview="handlePreview"
-          @change="handleChange"
-          name="avatar"
-        >
+        <a-upload :action="'http://mallapi.duyiedu.com/upload/images?appkey=' + $store.state.user.appkey"
+          list-type="picture-card" :file-list="fileList" @preview="handlePreview" @change="handleChange" name="avatar">
           <div v-if="fileList.length < 8">
             <a-icon :type="loading ? 'loading' : 'plus'" />
             <div class="ant-upload-text">Upload</div>
@@ -66,9 +54,18 @@ export default {
     };
   },
   props: ['form'],
+  created() {
+    if (this.form.images.length > 0) {
+      this.fileList = this.form.images.map((item, index) => ({
+        uid: index,
+        name: `image-${index}.png`,
+        status: 'done',
+        url: item,
+      }));
+    }
+  },
   methods: {
     handleChange({ file, fileList }) {
-      console.log(file);
       if (file.status === 'done') {
         // eslint-disable-next-line vue/no-mutating-props
         this.form.images.push(file.response.data.url);
